baea0cf4a296acee6a05fbf23b33b8977566f2a8
shalvink <42127319+shalvink@users.noreply.github.com>
2025-03-05 12:22:17 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+console.log("Hello from script.js!");
+
+// Example: Change the background color of the page
+document.body.style.backgroundColor = "lightblue";
+
+// Example: Display a message on the webpage
+document.body.innerHTML += "<p>This text was added by script.js!</p>";
